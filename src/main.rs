2ca3a728c3a21542c4142fdf76f9689aae9112a5
Kyle Lacy <kylelacy@kyle.space>
2025-11-25 00:32:23 -0800

    Revert "neighbor node" changes
    This reverts commit 4c52bcf0a3fc6ecc98d33d405ea0b7bd590308b2.
    
    I was experimenting with mulitple targets per geoproximity record, but
    I don't think this is worth keeping due to a number of issues:
    
    - `external-dns` mangles the ordering, which would need to be fixed
    - We would need to account for node status to remove failing nodes
    - The behavior takes a while to reach equillibrium, since the first node
      won't have any neighbors, the second will have at most one, etc.
    - ...which means we would probably need to watch nodes, which starts to
      get more complex quickly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeSet;
 
 use clap::Parser;
-use joinery::{Joinable as _, JoinableIterator as _};
+use joinery::Joinable as _;
 use k8s_openapi::api::core::v1::Node;
 use miette::{Context as _, IntoDiagnostic as _};
 use network_interface::NetworkInterfaceConfig as _;
@@ -112,22 +112,8 @@ async fn update_nodes() -> miette::Result<()> {
         .wrap_err("$KUBE_NODE_NAME must be set")?;
     let node_annotation_ips = std::env::var("KUBE_NODE_ANNOTATION_IPS").ok();
     let node_annotation_node_name = std::env::var("KUBE_NODE_ANNOTATION_NODE_NAME").ok();
-    let node_annotation_target = std::env::var("KUBE_NODE_ANNOTATION_TARGET").ok();
-    let node_annotation_coordinates = std::env::var("KUBE_NODE_ANNOTATION_COORDINATES").ok();
-    let node_annotation_target_num_neighbors: usize =
-        std::env::var("KUBE_NODE_ANNOTATION_TARGET_NUM_NEIGHBORS")
-            .ok()
-            .map(|num_neighbors| num_neighbors.parse())
-            .transpose()
-            .into_diagnostic()
-            .wrap_err("invalid value for $KUBE_NODE_ANNOTATION_TARGET_NUM_NEIGHBORS")?
-            .unwrap_or(0);
-    let node_annotation_target_neighbor_label_selector =
-        std::env::var("KUBE_NODE_ANNOTATION_TARGET_NEIGHBOR_LABEL_SELECTOR").ok();
     miette::ensure!(
-        node_annotation_ips.is_some()
-            || node_annotation_node_name.is_some()
-            || node_annotation_target.is_some(),
+        node_annotation_ips.is_some() || node_annotation_node_name.is_some(),
         "no annotations are enabled"
     );
 
@@ -185,128 +171,6 @@ async fn update_nodes() -> miette::Result<()> {
                     annotation,
                     "node name annotation already up-to-date"
                 );
-            }
-        }
-
-        if let Some(target_annotation) = &node_annotation_target {
-            let global_ips = get_ips().await?;
-            let mut targets = vec![global_ips.iter().join_with(",").to_string()];
-
-            if node_annotation_target_num_neighbors > 0 {
-                // Find the nearest neighbor nodes to add to the target too
-
-                let ips_annotation = node_annotation_ips
-                    .as_ref()
-                    .wrap_err("$KUBE_NODE_ANNOTATION_IPS must be set with $NODE_ANNOTATION_TARGET_NUM_NEIGHBORS")?;
-
-                let coordinates = node_annotation_coordinates.as_ref().and_then(|annotation| {
-                    let coordinates = node_coordinates(node_entry.get(), annotation)
-                        .inspect_err(|err| {
-                            tracing::warn!(
-                                node = node_name,
-                                annotation,
-                                "invalid coordinates on node: {err:?}"
-                            );
-                        })
-                        .ok()??;
-                    Some((annotation, coordinates))
-                });
-                if let Some((coordinates_annotation, node_coords)) = coordinates {
-                    let mut node_list_params = kube::api::ListParams::default();
-                    if let Some(label_selector) = &node_annotation_target_neighbor_label_selector {
-                        // Filter with label selector, if provided
-                        node_list_params = node_list_params.labels(&label_selector);
-                    }
-
-                    let node_list = nodes
-                        .list(&node_list_params)
-                        .await
-                        .into_diagnostic()
-                        .wrap_err("failed to list nodes")?;
-
-                    // Get other nodes with coordinates and IPs set
-                    let mut neighbors = node_list
-                        .items
-                        .into_iter()
-                        .filter(|neighbor| {
-                            // Exclude the current node
-                            neighbor.metadata.name.as_ref() != Some(&node_name)
-                        })
-                        .filter_map(|neighbor| {
-                            // Consider only nodes with both IP and coordinate
-                            // annotations
-                            let neighbor_ips = neighbor
-                                .metadata
-                                .annotations
-                                .as_ref()
-                                .and_then(|annotations| annotations.get(ips_annotation))?
-                                .clone();
-                            let neighbor_coords =
-                                node_coordinates(&neighbor, &coordinates_annotation)
-                                    .ok()
-                                    .flatten()?;
-                            let dist = distance_between_coordinates(node_coords, neighbor_coords);
-
-                            Some((neighbor_ips, neighbor_coords, dist))
-                        })
-                        .collect::<Vec<_>>();
-
-                    // Sort nodes by distance from the current node
-                    neighbors.sort_by_cached_key(|(_, _, dist)| {
-                        ordered_float::NotNan::new(*dist).unwrap()
-                    });
-
-                    if neighbors.len() > 0 {
-                        tracing::info!(
-                            ?global_ips,
-                            ?neighbors,
-                            node_annotation_target_num_neighbors,
-                            "found extra neighbors to add to target"
-                        )
-                    } else {
-                        tracing::info!(
-                            ?global_ips,
-                            node_annotation_target_num_neighbors,
-                            "no extra neighbors found to add to target"
-                        );
-                    }
-
-                    // Take only the desired number of neighbors
-                    neighbors.truncate(node_annotation_target_num_neighbors);
-                    let neighbor_ips = neighbors
-                        .into_iter()
-                        .map(|(neighbor_ips, _, _)| neighbor_ips)
-                        .take(node_annotation_target_num_neighbors)
-                        .collect::<Vec<_>>();
-                    targets.extend(neighbor_ips);
-                } else if let Some(annotation) = &node_annotation_coordinates {
-                    tracing::warn!(
-                        node_annotation_target_num_neighbors,
-                        annotation,
-                        "coordinates annotation not set for node, ignoring neighbors"
-                    );
-                } else {
-                    miette::bail!(
-                        "configured to set neighbors, but $KUBE_NODE_ANNOTATION_TARGET_NUM_NEIGHBORS must be > 0"
-                    );
-                }
-            }
-
-            let target_list = targets.join_with(",").to_string();
-            if set_annotation(node_entry, &target_annotation, &target_list) {
-                tracing::info!(
-                    node = node_name,
-                    target_list,
-                    annotation = target_annotation,
-                    "updated node annotation with targets"
-                );
-            } else {
-                tracing::info!(
-                    node = node_name,
-                    target_list,
-                    annotation = target_annotation,
-                    "node target annotation already up-to-date"
-                )
             }
         }
 
@@ -387,69 +251,5 @@ async fn update_nodes_repeatedly(every: std::time::Duration) -> miette::Result<(
                 // Ready to update IPs again
             }
         }
-    }
-}
-
-fn node_coordinates(node: &Node, annotation: &str) -> miette::Result<Option<Coordinates>> {
-    let Some(annotations) = node.metadata.annotations.as_ref() else {
-        return Ok(None);
-    };
-
-    let Some(coordinates) = annotations.get(annotation) else {
-        return Ok(None);
-    };
-
-    let coordinates: Coordinates = coordinates.parse()?;
-    Ok(Some(coordinates))
-}
-
-const EARTH_RADIUS: f32 = 6371.0;
-
-fn distance_between_coordinates(a: Coordinates, b: Coordinates) -> f32 {
-    let delta_lat = (b.lat_rad() - a.lat_rad()).abs();
-    let delta_lon = (b.lon_rad() - a.lon_rad()).abs();
-    let hav_delta_lat = (1.0 - f32::cos(delta_lat)) / 2.0;
-    let hav_delta_lon = (1.0 - f32::cos(delta_lon)) / 2.0;
-    let hav_great_circle_dist =
-        hav_delta_lat + (f32::cos(a.lat_rad()) * f32::cos(b.lat_rad()) * hav_delta_lon);
-    let great_circle_dist = 2.0 * f32::asin(f32::sqrt(hav_great_circle_dist));
-    great_circle_dist * EARTH_RADIUS
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Coordinates {
-    latitude: f32,
-    longitude: f32,
-}
-
-impl Coordinates {
-    fn lat_rad(&self) -> f32 {
-        self.latitude.to_radians()
-    }
-
-    fn lon_rad(&self) -> f32 {
-        self.longitude.to_radians()
-    }
-}
-
-impl std::str::FromStr for Coordinates {
-    type Err = miette::Report;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let coordinates = s.split_once(",").and_then(|(lat, lon)| {
-            let lat: f32 = lat.parse().ok()?;
-            let lon: f32 = lon.parse().ok()?;
-            Some((lat, lon))
-        });
-        let Some((latitude, longitude)) = coordinates else {
-            miette::bail!(
-                "invalid coordinates: expected a string of the form 'latitude,longitude', got {s:?}"
-            );
-        };
-
-        Ok(Self {
-            latitude,
-            longitude,
-        })
     }
 }
